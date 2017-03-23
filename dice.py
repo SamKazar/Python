53a95c3f5f542e1798b2ac77ffe27e74df621cf9
SamKazar <samkazar17@gmail.com>
2017-03-23 18:15:49 +0000

    Update dice.py
    Added functionality to choose number of dice and number of sides on those dice via system argument
    if no arguments are passed then two six sided dice are rolled (as in original file)

--- dice.py
+++ dice.py
@@ -1,20 +1,32 @@
 # Script Name	: dice.py
 # Author		: Craig Richards
 # Created		: 05th February 2017
-# Last Modified	: 
-# Version		: 1.0
-
-# Modifications	:
-
 # Description	: This will randomly select two numbers, like throwing dice, you can change the sides of the dice if you wish
 
-import random
-class Die(object):
-  def __init__(self, sides):
-    self.sides = sides
-  def roll(self):
-    return random.randint(1, self.sides)
+# Modifications	: Added functionality to choose number of dice and number of sides on those dice by system argument,
+#               : if no arguments are passed then two six sided dice are rolled (as in original file)
+# Last Modified	: 23rd March 2017
+# Modified by   : Sam Kazar
+# Version		: 2.0
 
-d = Die(6)
-d1 = Die(6)
-print d.roll(), d1.roll()
+import random
+import sys
+
+try:
+    num_of_dice = int(sys.argv[1])
+    num_of_sides = int(sys.argv[2])
+
+except (IndexError, ValueError):
+    num_of_dice = 2
+    num_of_sides = 6
+
+class Die(object):
+
+    def __init__(self, sides):
+        self.sides = sides
+
+    def roll(self):
+        return random.randint(1, self.sides)
+
+for x in range(0, num_of_dice):
+    print(Die(num_of_sides).roll())
